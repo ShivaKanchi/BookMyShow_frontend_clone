c32702eb9e66c0260846b6546a0b609df60ce24f
ShivaKanchi <shivakanchi111@gmail.com>
2022-10-23 22:55:10 +0530

    Hero page

--- src/components/HeroCarousel/HeroCarousel.Component.jsx
+++ src/components/HeroCarousel/HeroCarousel.Component.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import HeroSlider from "react-slick";
 const HeroCarousel = () => {
+
+    const [images, setImages] = useState([]);
+
     return (
-        <div>HeroCarousel</div>
-    )
+        <>
+            <div>HeroCarousel</div>
+            <div className='lg:hidden '>
+                <HeroSlider>
+                    {
+                        images.map((images) => (
+                            <div className='w-full h-56 md:h-80 py-3'>
+                                <img
+                                    src={`https://image.tmdb.org/t/p/original${images.backdrop_path}`}
+                                    alt='Hero carousel Banner'
+                                    className='w-full rounded-md object-cover' />
+                            </div>
+                        ))
+                    }
+                </HeroSlider>
+            </div>
+            <div className='hidden lg:block'>
+                <HeroSlider>
+                    {
+                        images.map((images) => (
+                            <div className='w-full h-96 px-2 py-3'>
+                                <img
+                                    src={`https://image.tmdb.org/t/p/original${images.backdrop_path}`}
+                                    alt='Hero carousel Banner'
+                                    className='w-full rounded-md object-cover' />
+                            </div>
+                        ))
+                    }
+                </HeroSlider>
+            </div>
+        </>
+    );
+
 }
 
 export default HeroCarousel
