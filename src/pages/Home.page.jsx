7185ace739f1c003d7ad3d690a9d9f6b62245957
ShivaKanchi <shivakanchi111@gmail.com>
2022-10-23 16:34:03 +0530

    Hoc edit

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+//Components
+import EntertainmentCard from '../components/Entertainment/EntertainmentCard.component'
+import HeroCarousel from '../components/HeroCarousel/HeroCarousel.Component'
+import PosterSlider from '../components/PosterSlider/PosterSlider.Component'
+//layout Hoc
 import DefaultLayoutHoc from '../layout/Default.layout'
+
 const HomePage = () => {
     return (
         <div>HomePage</div>
