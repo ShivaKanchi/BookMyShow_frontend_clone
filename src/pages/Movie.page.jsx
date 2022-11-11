627744bec02400b67b3df371689c85693cf97aa9
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-11 22:35:30 +0530

    Context provider added

--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import MovieLayoutHoc from '../layout/Movie.layout';
-
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 const MoviePage = () => {
+    const { id } = useParams();
+    const [cast, setCast] = useState([]);
+    const [similarMovies, setSimilarMovies] = useState([]);
+    const [recommendedMovies, setRecommendedMovies] = useState([]);
+
+    useEffect(() => {
+        const requestCast = async () => {
+            const getCast = await axios.get(`/movie/${id}/credits`);
+            setCast(getCast.data.cast);
+        };
+        requestCast();
+    }, [id]);
+
+    useEffect(() => {
+        const requestSimilarMovies = async () => {
+            const getSimilarMovies = await axios.get(`/movie/${id}/similar`);
+            setSimilarMovies(getSimilarMovies.data.results);
+        };
+        requestSimilarMovies();
+    }, [id]);
+
+    useEffect(() => {
+        const requestRecommendedMovies = async () => {
+            const getRecommendedMovies = await axios.get(`/movie/${id}/similar`);
+            setRecommendedMovies(getRecommendedMovies.data.results);
+        };
+        requestRecommendedMovies();
+    }, [id]);
+
     return (
         <div>MoviePage</div>
-    )
+    );
 }
 
 export default MovieLayoutHoc(MoviePage);
